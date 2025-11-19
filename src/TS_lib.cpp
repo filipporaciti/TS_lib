baede71f612286f427b07dd0235336292b31e03c
filipporaciti <filipporaciti15@libero.it>
2025-11-19 11:48:24 +0100

    implement serial receive when update

--- src/TS_lib.cpp
+++ src/TS_lib.cpp
@@ -5,9 +5,18 @@
 TS_lib::TS_lib(const Stream* s1) {
 	_serial1 = s1;
 	_comm = Communication(_serial1);
+	_comm_legacy = Communication_legacy(_serial1);
 }
 
 void TS_lib::update() {
+
+	if (_serial1->available() > 0) {
+		if (_comm_legacy.isLegacy(_serial1->peek())) {
+			_comm_legacy.serialReceiveLegacy();
+		} else {
+			_comm.serialReceive();
+		}
+	}
 
 }
 void TS_lib::setRealTimeStruct(const void* rt_data, const size_t structLen) {
