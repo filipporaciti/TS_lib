27edf0cd16c28077b2b9ee0329a2e28dd9ee8e4c
filipporaciti <filipporaciti15@libero.it>
2025-11-19 13:47:21 +0100

    impl getPageValue

--- src/pages.cpp
+++ src/pages.cpp
@@ -5,7 +5,10 @@ Pages::Pages(const Page* pages) {
 }
 
 void* Pages::getPageValue(uint16_t pageNum, uint16_t offset){
-
+	if (pageNum >= (sizeof(_pages)/sizeof(_pages[0]))) {
+		return nullptr;
+	}
+	return (uint8_t*)&_pages[0].pointer + offset;
 }
 
 uint32_t Pages::getPageCRC(const void *page, size_t pageLen){
