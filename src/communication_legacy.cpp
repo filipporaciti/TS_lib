af6c5e1a74e05cd54d40b74326daf7cf55fc3f79
filipporaciti <filipporaciti15@libero.it>
2025-11-19 11:41:41 +0100

    fix constructor in comm legacy + ref

--- src/communication_legacy.cpp
+++ src/communication_legacy.cpp
@@ -2,7 +2,9 @@
 
 
 Communication_legacy::Communication_legacy(const Stream* serial) {
-	Communication_legacy(serial, DEFAULT_CODE_VERSION);
+	_code_version = DEFAULT_CODE_VERSION;
+	_protocol_version = DEFAULT_PROTOCOL_VERSION;
+	_serial = serial;
 }
 Communication_legacy::Communication_legacy(const Stream* serial, const char* code_version) {
 	_code_version = code_version;
@@ -12,8 +14,8 @@ Communication_legacy::Communication_legacy(const Stream* serial, const char* cod
 
 void Communication_legacy::serialReceiveLegacy() {
 
-	if (_serial->available() > 0) {
-		uint8_t serialCmd = _serial->read();
+	if (_serial->available()) {
+		char serialCmd = (char)_serial->read();
 		switch (serialCmd) {
 			case 'Q': {
 				_serial->print(_code_version);
