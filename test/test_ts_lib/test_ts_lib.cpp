95495e21b98510d9209c9ebdeb79f32e7f03d162
filipporaciti <filipporaciti15@libero.it>
2026-02-25 19:14:30 +0100

    GREEN: test set code version

--- test/test_ts_lib/test_ts_lib.cpp
+++ test/test_ts_lib/test_ts_lib.cpp
@@ -17,10 +17,18 @@ void test_code_version(void) {
   TEST_ASSERT_EQUAL_STRING(DEFAULT_CODE_VERSION, code_version);
 }
 
+void test_set_code_version(void) {
+  char new_code_version[] = "ds798DSAGD%&$/ds";
+  ts.setCodeVersion(new_code_version);
+  char* code_version = ts.getCodeVersion();
+  TEST_ASSERT_EQUAL_STRING(new_code_version, code_version);
+}
+
 void setup() {
   UNITY_BEGIN();
 
   RUN_TEST(test_code_version);
+  RUN_TEST(test_set_code_version);
 
   UNITY_END();
 }
