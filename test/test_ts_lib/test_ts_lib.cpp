88dd82e0377b1070cabcac78815d149a32b835a2
filipporaciti <filipporaciti15@libero.it>
2026-02-25 19:20:23 +0100

    GREEN: test code version limit

--- test/test_ts_lib/test_ts_lib.cpp
+++ test/test_ts_lib/test_ts_lib.cpp
@@ -32,12 +32,20 @@ void test_code_version_ref_escape(void) {
   TEST_ASSERT_EQUAL_STRING("ds798DSAGD%&$/ds", code_version);
 }
 
+void test_code_version_limit(void) {
+  char long_code_version[] = "1234567890123456789012345678901234567890123456789012345678901234";
+  ts.setCodeVersion(long_code_version);
+  char* code_version = ts.getCodeVersion();
+  TEST_ASSERT_EQUAL_STRING("123456789012345678901234567890123456789012345678901234567890123", code_version);
+}
+
 void setup() {
   UNITY_BEGIN();
 
   RUN_TEST(test_code_version);
   RUN_TEST(test_set_code_version);
   RUN_TEST(test_code_version_ref_escape);
+  RUN_TEST(test_code_version_limit);
 
   UNITY_END();
 }
