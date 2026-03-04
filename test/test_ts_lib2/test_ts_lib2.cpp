389c417ccae8029d11dd5a2bf0f53661551f7f28
filipporaciti <filipporaciti15@libero.it>
2026-03-04 15:05:32 +0100

    GREEN: test update

--- test/test_ts_lib2/test_ts_lib2.cpp
+++ test/test_ts_lib2/test_ts_lib2.cpp
@@ -21,12 +21,23 @@ void test_update_legacy(void) {
   TEST_ASSERT_EQUAL_STRING(DEFAULT_CODE_VERSION, mockStream.getTxBuffer());
 }
 
+void test_update(void) {
+  uint8_t data[] = {0x00, 0x01, 0x51, 0xCE, 0x6E, 0x8E, 0xEF};
+  uint8_t expected_len = 2 + 1 + 13 + 4;
+
+  mockStream.pushBytes(data, sizeof(data));
+
+  TEST_ASSERT_EQUAL(0, mockStream.getTxBufferLen());
+  ts.update();
+  TEST_ASSERT_EQUAL(expected_len, mockStream.getTxBufferLen());
+}
+
 
 void setup() {
   UNITY_BEGIN();
 
   RUN_TEST(test_update_legacy);
-//   RUN_TEST(test_update);
+  RUN_TEST(test_update);
   // RUN_TEST(test_update_not_ready);
   // RUN_TEST(test_update_empty);
 
